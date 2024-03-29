48dd6fb119ac92e5cca7cf2f4b81c7643bc7c5a1
sounwoo <mna800@naver.com>
2024-03-29 23:37:16 +0900

    spring boot testCode (연습)

--- practice/src/main/java/com/example/practice/board/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.practice.board;
-
-import com.example.practice.board.entity.BoardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//Jpa(데이터 type, Pk type)
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-}

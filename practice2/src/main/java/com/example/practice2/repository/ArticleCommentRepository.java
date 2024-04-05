e26077d97bdf1c867620443665e3917353e07e33
sounwoo <mna800@naver.com>
2024-04-05 17:51:25 +0900

    querydsl - 검색

--- practice2/src/main/java/com/example/practice2/repository/ArticleCommentRepository.java
+++ practice2/src/main/java/com/example/practice2/repository/ArticleCommentRepository.java
@@ -1,9 +1,27 @@
 package com.example.practice2.repository;
 
 import com.example.practice2.domain.ArticleComment;
+import com.example.practice2.domain.QArticle;
+import com.example.practice2.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
-@Repository
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createAt, root.createBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createBy).first(StringExpression::containsIgnoreCase);
+    }
 }
--- practice2/src/main/java/com/example/practice2/repository/ArticleRepository.java
+++ practice2/src/main/java/com/example/practice2/repository/ArticleRepository.java
@@ -1,9 +1,26 @@
 package com.example.practice2.repository;
 
 import com.example.practice2.domain.Article;
+import com.example.practice2.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
-@Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title,root.content, root.hashtag, root.createAt, root.createBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
